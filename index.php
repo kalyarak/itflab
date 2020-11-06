0b4cbe78e136146dec229d97b39cc09b64a00cbe
kalyarak <73679596+kalyarak@users.noreply.github.com>
2020-11-06 10:44:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,28 @@
-<?php
-echo "<h2>PHP is Fun!</h2>";
-echo "Hello World!<br>";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>LAB01</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <img src="img/creative/creative5.jpg" style="height: 600px width: 600px" class="img-fluid">
+            </div>
+            <div class="col-lg-2">
+                <img src="img/creative/creative.jpg" style="height: 180px" class="img-fluid" />
+                <img src="img/creative/creative3.jpg" style="height: 180px" class="img-fluid" />
+                <img src="img/creative/creative4.jpg" style="height: 180px" class="img-fluid" />
+            </div>
+            <div class="col-lg-3">
+                <img src="img/creative/creative2.jpg" style="height: 270px" class="img-fluid" />
+                <img src="img/creative/creative6.jpg" style="height: 270px" class="img-fluid" />
+            </div>
+            </div>
+</body>
+</html>
